e2fd8b4a5fafccbefdeb8a9de43075472ab220ce
xlsanchez <sandovalxavier0812@gmail.com>
2025-12-10 18:29:25 +0000

    list1000

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,47 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# While loops  
+# for loops = execute a block of code a fixed number of times 
+# you can iterate over a range string sequence etc. 
+
+
+for x in reversed(range(1, 11)):
+   print(x)
+
+print("Happy New Year!") 
+
+
+
+for x in range(1, 11, 2):
+   print(x)
+
+
+   credit_card = "1234-5678-9012-3456"
+
+for x in credit_card:
+   print(x)  
+
+   for x in range(1, 21):
+      if x == 13:
+       continue
+   else:
+       print(x)
+
+
+       
+for x in range(1, 21):
+   if x == 13:
+       break
+   else:
+       print(x)
+
+
+#while loops = execute some code WHILE some conditiion remiands true 
+
+name = input ("Enter your name: ")
+while name == "": 
+   print("You did not enter your name.")
+   name = input("Enter your name: ")
+else: 
+   print(f"Hello {name}")
+
+age = int(input("Enter "))
