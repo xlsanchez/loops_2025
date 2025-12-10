bc2065142befa0f3e2542010c102361910eba35c
xlsanchez <sandovalxavier0812@gmail.com>
2025-12-10 19:30:34 +0000

    3

--- 3_looping_over_string.py
+++ 3_looping_over_string.py
@@ -4,3 +4,16 @@
 
 # Challenge:
 # Count how many vowels are in the word.
+
+
+word = input("Enter the word")
+for letter in word: 
+    print(letter)
+
+vowels ="aeiouAEIOU"
+count = 0
+
+for letter in word: 
+    if letter in vowels: 
+        count +=1 
+print("number of vowels: ", count )
